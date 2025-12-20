3728ad49e67ebfb792f0226c4f6555190b448793
Reimu0826 <powerfullerror01@gmail.com>
2025-12-20 10:34:01 +0900

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,18 +1,28 @@
 import streamlit as st
 import random
 
-# 질문지 세트 데이터 구성이 중요, 심플하게 반복문으로 구현 가능
+# 질문지 세트
 quiz = [
-    ["교목은?", ["느티나무", "소나무"]],
-    ["상징 동물은?", ["동호", "토끼"]],
-    ['교화는?', ['동백꽃', '무궁화']],
-    ['빈 칸에 들어갈 산은?', ['백두산', '금정산']],
-    ['우리 학교에 있는 석상은?', ['image01.png', 'image02.png']]
+    ["교목은?", ["느티나무", "소나무"], "느티나무"],
+    ["상징 동물은?", ["동호", "토끼"], "동호"],
+    ['교화는?', ['동백꽃', '무궁화'], "동백꽃"],
+    ['빈 칸에 들어갈 산은?', ['백두산', '금정산'], "백두산"],
+    ['우리 학교에 있는 석상은?', ['image01.png', 'image02.png'], "image01.png"]
 ]
 
 random.shuffle(quiz)
 
 for no in quiz:
-    st.write(no[0])
-    answer = st.text_input(no[0])
-    # 정답이면
+    question = no[0]
+    choices = no[1]
+    correct = no[2]
+
+    st.write(question)
+
+    answer = st.radio("보기 선택", choices, key=question)
+
+    if st.button("정답 제출", key=question + "_btn"):
+        if answer == correct:
+            st.success("정답입니다!")
+        else:
+            st.error("오답입니다.")
