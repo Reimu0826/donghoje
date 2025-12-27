9788f84a4d06704d0a64cbd6e0b99f591ce1bfc5
bs-comscience <comscience@bsnamil.hs.kr>
2025-12-27 14:12:32 +0900

    Update code1.py

--- code1.py
+++ code1.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_icon="mark.png"
 )
 
-ACCESS_CODE = "48201" 
+ACCESS_CODE = "1004" 
 IMAGE_BASE_URL = "https://raw.githubusercontent.com/Reimu0826/donghoje/main/image/"
 IMAGE_WIDTH = 300
 
